ab89f798fe362c7ff02de1a826b16c61e90d4c7d
Aditi Green <asaki444@gmail.com>
2018-05-08 21:27:43 -0400

    added div to the component

--- testreactapp/src/components/WelcomePage.js
+++ testreactapp/src/components/WelcomePage.js
@@ -1,7 +1,9 @@
-
 import React, { Component } from 'react';
 
 function WelcomePage(){
 	return(
+      <div className="Welcome">
+      		<h1 className="Welcome"> Welcome to My Test App</h1>
+      </div>
 	)
 }
